47befa933f1a57765c839c99d8ff7278ab529bb7
Collin Robinson <collin@cjgraphix.com>
2020-05-12 15:18:11 -0700

    Auth Screen
    CSS / HTML

--- front-end/src/components/LoginBanner.js
+++ front-end/src/components/LoginBanner.js
@@ -3,10 +3,30 @@ import React from 'react';
 const LoginBanner = () => (
   <div className="applogin-banner">
     <div className="applogin-background" />
-    {/* <div className="applogin-container">
-      <h1>Connecting to FETT Portal</h1>
-      <p>Sign-in to you account to access FETT Portal</p>
-    </div> */}
+    <div className="applogin-container">
+      <h1>
+        <span>FETT Portal</span>
+      </h1>
+      <p>Sign in to access FETT Portal</p>
+
+      <form>
+        <div className="applogin-formfield">
+          <label htmlFor="fett-username">
+            User Name
+            <input type="text" name="username" id="fett-username" />
+          </label>
+        </div>
+
+        <div className="applogin-formfield">
+          <label htmlFor="fett-password">
+            Password
+            <input type="password" name="password" id="fett-password" />
+          </label>
+        </div>
+
+        <button type="submit">LOGIN</button>
+      </form>
+    </div>
   </div>
 );
 export default LoginBanner;
