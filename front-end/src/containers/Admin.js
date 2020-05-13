56d7ba1db401d4c6d9e831aac95ba6a0f008d88b
alex-fivetalent <alex.hanson@fivetalent.com>
2020-05-13 10:49:08 -0700

    58033971 Add Private Routes for portals and authcheck inside portal

--- front-end/src/containers/Admin.js
+++ front-end/src/containers/Admin.js
@@ -6,21 +6,33 @@ import Users from '../components/Users';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
 
-const Admin = ({ isAdmin }) => (
-  <div className="flex-col h-full portal-container">
-    <Header isAdmin={isAdmin} />
-    <div style={{ minHeight: 'calc(100vh - 6em)' }} className="flex flex-row">
-      <Sidebar isAdmin={isAdmin} />
-      <div className="bg-blue-700 w-full">
-        <Route path="/adminportal/dashboard" component={AdminDash} />
-        <Route path="/adminportal/users" component={Users} />
-        <Route exact path="/adminportal" render={() => <Redirect to="adminportal/dashboard" />} />
+const Admin = props => {
+  const { isAdmin, isLoggedIn } = props;
+  const checkAuth = isLoggedIn && isAdmin;
+
+  return (
+    <div className="flex-col h-full portal-container">
+      <Header isAdmin={isAdmin} />
+      <div style={{ minHeight: 'calc(100vh - 6em)' }} className="flex flex-row">
+        <Sidebar isAdmin={isAdmin} />
+        <div className="bg-blue-700 w-full">
+          {checkAuth ? (
+            <>
+              <Route path="/adminportal/dashboard" component={AdminDash} />
+              <Route path="/adminportal/users" component={Users} />
+              <Route path="/adminportal" render={() => <Redirect to="adminportal/dashboard" />} />
+            </>
+          ) : (
+            <Redirect to="/" />
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 export default Admin;
 
 Admin.propTypes = {
   isAdmin: PropTypes.bool,
+  isLoggedIn: PropTypes.bool,
 };
--- front-end/src/containers/MainRouter.js
+++ front-end/src/containers/MainRouter.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
-// import Home from './Home';
 import Login from './Login';
-// import LoginReceiver from './LoginReceiver';
-// import PrivateRoute from '../components/PrivateRoute';
+import PrivateRoute from '../components/PrivateRoute';
 import NoMatch from '../components/NoMatch';
 import Logout from '../components/Logout';
 import Admin from './Admin';
@@ -16,10 +14,8 @@ const MainRouter = props => {
   return (
     <Switch>
       <Route exact path="/" render={() => <Login login={login} />} />
-      {/* <Route path="/authorizing" render={rest => <LoginReceiver {...rest} auth={auth} setStorage={setStorage} />} /> */}
-      {/* <PrivateRoute path="/home" component={Home} isAdmin={isAdmin} {...props} /> */}
-      <Route path="/adminportal" render={() => <Admin {...props} />} />
-      <Route path="/bountyportal" render={() => <Bounty {...props} />} />
+      <PrivateRoute path="/adminportal" component={Admin} {...props} />
+      <PrivateRoute path="/bountyportal" component={Bounty} {...props} />
       <Route path="/logout" render={() => <Logout {...props} />} />
       <Route component={NoMatch} />
     </Switch>
