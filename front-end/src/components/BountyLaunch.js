9872ba02dd5e6318eadbd87ed29468bac79f373e
alex-fivetalent <alex.hanson@fivetalent.com>
2020-05-18 15:08:00 -0700

    58033958 Add bounty launch page

--- front-end/src/components/BountyLaunch.js
+++ front-end/src/components/BountyLaunch.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import LaunchTable from './LaunchTable';
 
 export default function BountyLaunch() {
-  return <div>Bounty Launch</div>;
+  return (
+    <div className="h-full pt-6 pl-12">
+      <h3 className="text-gray-200 uppercase">dashboard</h3>
+      <p className="pt-4 text-gray-200">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id consectetur
+        purus ut faucibus pulvinar elementum integer.
+      </p>
+      <div className="flex flex-row w-full mt-4">
+        <LaunchTable />
+      </div>
+    </div>
+  );
 }
