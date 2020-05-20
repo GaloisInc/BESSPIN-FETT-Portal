2524c50fb8fc55e7d3738e03e0d7ecfc96785dec
Brian McCall <brian.mccall@fivetalent.com>
2020-05-19 19:01:32 -0700

    fix migrations

--- back-end/migrations/20200504_inital_migration.js
+++ back-end/migrations/20200504_inital_migration.js
@@ -1,58 +1,64 @@
-exports.up = function(knex) {
+exports.up = async function(knex, Promise) {
   // example table
-  return Promise.all([
-    knex.schema.createTable('User', function(table) {
-      table.increments('Id').primary();
-      table.integer('CreatedBy').references('User.Id');
-      table.varchar('UserName', 255).notNull();
-      table.varchar('EmailAddress', 255).notNull();
-      table.varchar('Role', 255).notNull();
-      table
-        .boolean('IsActive')
-        .notNull()
-        .defaultTo(true);
-      table.timestamps(true, true);
-    }),
-    knex.schema.createTable('Announcement', function(table) {
-      table.increments('Id').primary();
-      table.integer('CreatedBy').references('User.Id');
-      table.varchar('Team', 255).notNull();
-      table.varchar('Type', 255).notNull();
-      table.text('Payload');
-      table
-        .boolean('IsActive')
-        .notNull()
-        .defaultTo(true);
-      table.timestamps(true, true);
-    }),
-    knex.schema.createTable('InstanceConfiguration', function(table) {
-      table.increments('Id').primary();
-      table.varchar('Type', 255).notNull();
-      table.varchar('Processor', 255).notNull();
-      table.varchar('OS', 255).notNull();
-      table
-        .boolean('IsActive')
-        .notNull()
-        .defaultTo(true);
-    }),
-    knex.schema.createTable('Environment', function(table) {
-      table.increments('Id').primary();
-      table.integer('CreatedBy').references('User.Id');
-      table.integer('Configuration').references('InstanceConfiguration.Id');
-      table.varchar('Region', 255).notNull();
-      table.varchar('Status', 255).notNull();
-      table.varchar('F1EnvironmentId', 255).notNull();
-      table.varchar('IpAddress', 255).notNull();
-      table.integer('IdleSeconds');
-      table.varchar('PrivateKeyStore', 255).notNull();
-      table.varchar('LoggingLocation', 255).notNull();
-      table
-        .boolean('IsActive')
-        .notNull()
-        .defaultTo(true);
-      table.timestamps(true, true);
-    }),
-  ]);
+  await knex.schema.createTable('User', table => {
+    table.increments('Id').primary();
+    table.integer('CreatedBy');
+    table.varchar('UserName', 255).notNull();
+    table.varchar('EmailAddress', 255).notNull();
+    table.varchar('Role', 255).notNull();
+    table
+      .boolean('IsActive')
+      .notNull()
+      .defaultTo(true);
+    table.timestamps(true, true);
+  });
+
+  await knex.schema.createTable('Announcement', table => {
+    table
+      .integer('CreatedBy')
+      .unsigned()
+      .notNull();
+    table.foreign('CreatedBy').references('User.Id');
+    table.varchar('Team', 255).notNull();
+    table.varchar('Type', 255).notNull();
+    table.text('Payload');
+    table
+      .boolean('IsActive')
+      .notNull()
+      .defaultTo(true);
+    table.timestamps(true, true);
+  });
+  await knex.schema.createTable('InstanceConfiguration', table => {
+    table.increments('Id').primary();
+    table.varchar('Type', 255).notNull();
+    table.varchar('Processor', 255).notNull();
+    table.varchar('OS', 255).notNull();
+    table
+      .boolean('IsActive')
+      .notNull()
+      .defaultTo(true);
+  });
+  await knex.schema.createTable('Environment', table => {
+    table
+      .integer('CreatedBy')
+      .unsigned()
+      .notNull();
+    table.foreign('CreatedBy').references('User.Id');
+    table.integer('Configuration').unsigned();
+    table.foreign('Configuration').references('InstanceConfiguration.Id');
+    table.varchar('Region', 255).notNull();
+    table.varchar('Status', 255).notNull();
+    table.varchar('F1EnvironmentId', 255).notNull();
+    table.varchar('IpAddress', 255).notNull();
+    table.integer('IdleSeconds');
+    table.varchar('PrivateKeyStore', 255).notNull();
+    table.varchar('LoggingLocation', 255).notNull();
+    table
+      .boolean('IsActive')
+      .notNull()
+      .defaultTo(true);
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex) {
