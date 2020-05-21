15766d2a2d8e3b65e4875c30757a01f5953715be
alex-fivetalent <alex.hanson@fivetalent.com>
2020-05-20 17:18:11 -0700

    58278237 Remove mysql package and hookup db with knex

--- back-end/src/helpers/database.js
+++ back-end/src/helpers/database.js
@@ -1,98 +1,27 @@
-const mysql = require('mysql');
-const knexFile = require('../../knexfile');
-const SsmHelper = require('./ssm');
+const knex = require('knex');
+const knexConfig = require('../../knexfile');
 
 class Database {
   constructor() {
     this.pool = null;
-  }
-
-  static connectionObject(connectionInfo) {
-    return {
-      host: connectionInfo.endpoint,
-      user: connectionInfo.user,
-      password: connectionInfo.password,
-      database: connectionInfo.database,
-      connectionLimit: 100,
-    };
-  }
-
-  static async getConnection() {
-    if (process.env.IS_OFFLINE === true) {
-      return Database.connectionObject({
-        ...knexFile.offline.connection,
-        endpoint: knexFile.offline.connection.host,
-      });
-    }
-    try {
-      // eslint-disable-next-line
-      const connectionInfo = await SsmHelper.getParametersByPath(`/fettportal/${process.env.stage}/db-aurora/`);
-      return Database.connectionObject({
-        ...connectionInfo,
-        database: knexFile[process.env.stage].connection.database,
-      });
-    } catch (e) {
-      console.log(`get connection: ${e}`);
-      throw e;
-    }
+    this.config = null;
   }
 
   async makeConnection() {
     return new Promise(async (resolve, reject) => {
-      let pool;
-      let conn;
       if (this.pool !== null) {
-        return resolve(this.pool);
-      }
-      try {
-        conn = await Database.getConnection();
-      } catch (e) {
-        console.log(`make connection: ${e}`);
-
-        throw e;
-      }
-      if (conn === undefined) {
-        throw new Error('Connection Undefined');
-      }
-      try {
-        pool = mysql.createPool(conn);
-      } catch (e) {
-        console.log('connection info', conn);
-        return reject(new Error('Could not make pool connection'));
-      }
-      // Ping database to check for common exception errors.
-      pool.getConnection((err, connection) => {
-        if (err) {
-          console.log(connection);
-          if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.error('Database connection was closed.');
-          }
-          if (err.code === 'ER_CON_COUNT_ERROR') {
-            console.error('Database has too many connections.');
-          }
-          if (err.code === 'ECONNREFUSED') {
-            console.error('Database connection was refused.');
-          }
-          return reject(err);
-        }
-        if (connection) {
-          connection.release();
-        }
-        this.pool = connection;
         return resolve();
-      });
+      }
+      this.connection = null;
+      this.config = await knexConfig();
+      this.pool = knex(this.config);
+      return resolve();
     });
   }
 
   async query(query) {
-    return new Promise((resolve, reject) => {
-      this.pool.query(query, (err, data) => {
-        if (err) {
-          return reject(err);
-        }
-        return resolve(data);
-      });
-    });
+    console.log('querying');
+    return this.pool.raw(query).then(data => data[0]);
   }
 }
 module.exports = Database;
