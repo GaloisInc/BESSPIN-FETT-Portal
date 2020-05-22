32967f884a8a4f04b0e9c8289cd14143e8e88ecb
alex-fivetalent <alex.hanson@fivetalent.com>
2020-05-22 15:22:28 -0700

    58033958 Add DB call and fix api function with promise

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -23,91 +23,99 @@ function handleErrors(response) {
   return response;
 }
 
-export const GetFromDatabase = () => {
-  console.log(BASE_API);
-  const query = JSON.stringify({ query: `SELECT * FROM Users` });
-  fetch(`${BASE_API}/getFromDatabase`, {
-    headers: makeHeaders(),
-    body: query,
-  })
-    .then(handleErrors)
-    .then(response => response.json())
-    .then(body => body)
-    .catch(response => {
-      throw new Error(response.json());
-    });
-};
+export const GetFromDatabase = () =>
+  new Promise(async (resolve, reject) => {
+    console.log(BASE_API);
+    const query = JSON.stringify({ query: `SELECT * FROM Users` });
+    fetch(`${BASE_API}/getFromDatabase`, {
+      headers: makeHeaders(),
+      body: query,
+    })
+      .then(handleErrors)
+      .then(response => response.json())
+      .then(body => resolve(body))
+      .catch(response => {
+        reject(response.json());
+      });
+  });
 
-export const createUser = async (email, role) => {
-  const sesh = await Auth.currentSession();
-  const myUserName = sesh.getAccessToken().payload.username;
+export const createUser = async (email, role) =>
+  new Promise(async (resolve, reject) => {
+    const sesh = await Auth.currentSession();
+    const myUserName = sesh.getAccessToken().payload.username;
 
-  fetch(`${BASE_API}/createUser`, {
-    headers: makeHeaders(),
-    body: JSON.stringify({ email: `${email}`, role: `${role}`, myUserName: `${myUserName}` }),
-  })
-    .then(handleErrors)
-    .then(response => response.json())
-    .then(body => body)
-    .catch(response => {
-      throw new Error(response.json());
-    });
-};
+    fetch(`${BASE_API}/createUser`, {
+      headers: makeHeaders(),
+      body: JSON.stringify({ email: `${email}`, role: `${role}`, myUserName: `${myUserName}` }),
+    })
+      .then(handleErrors)
+      .then(response => response.json())
+      .then(body => resolve(body))
+      .catch(response => {
+        reject(response.json());
+      });
+  });
 
-export const disableUser = email => {
-  fetch(`${BASE_API}/disableUser`, {
-    headers: makeHeaders(),
-    body: JSON.stringify({ email: `${email}` }),
-  })
-    .then(handleErrors)
-    .then(response => response.json())
-    .then(body => body)
-    .catch(response => {
-      throw new Error(response.json());
-    });
-};
+export const disableUser = email =>
+  new Promise(async (resolve, reject) => {
+    fetch(`${BASE_API}/disableUser`, {
+      headers: makeHeaders(),
+      body: JSON.stringify({ email: `${email}` }),
+    })
+      .then(handleErrors)
+      .then(response => response.json())
+      .then(body => resolve(body))
+      .catch(response => {
+        reject(response.json());
+      });
+  });
 
-export const getInstanceConfigurations = () => {
-  fetch(`${BASE_API}/getInstanceConfigurations`, {
-    headers: makeHeaders(),
-  })
-    .then(handleErrors)
-    .then(response => response.json())
-    .then(body => body)
-    .catch(response => {
-      throw new Error(response.json());
-    });
-};
+export const getInstanceConfigurations = () =>
+  new Promise(async (resolve, reject) => {
+    fetch(`${BASE_API}/getInstanceConfigurations`, {
+      headers: makeHeaders(),
+    })
+      .then(handleErrors)
+      .then(response => response.json())
+      .then(body => {
+        resolve(body.items);
+      })
+      .catch(response => {
+        reject(response.json());
+      });
+  });
 
-export const getEnvironments = () => {
-  fetch(`${BASE_API}/getEnvironments`, {
-    headers: makeHeaders(),
-  })
-    .then(handleErrors)
-    .then(response => response.json())
-    .then(body => body)
-    .catch(response => {
-      throw new Error(response.json());
-    });
-};
+export const getEnvironments = () =>
+  new Promise(async (resolve, reject) => {
+    fetch(`${BASE_API}/getEnvironments`, {
+      headers: makeHeaders(),
+    })
+      .then(handleErrors)
+      .then(response => response.json())
+      .then(body => resolve(body))
+      .catch(response => {
+        reject(response.json());
+      });
+  });
 
-export const createEnvironmentRecord = async configuration => {
-  const sesh = await Auth.currentSession();
-  const myUserName = sesh.getAccessToken().payload.username;
+export const createEnvironmentRecord = async configuration =>
+  new Promise(async (resolve, reject) => {
+    const sesh = await Auth.currentSession();
+    const myUserName = sesh.getAccessToken().payload.username;
 
-  fetch(`${BASE_API}/createEnvironment`, {
-    headers: makeHeaders(),
-    body: JSON.stringify({
-      OS: `${configuration.OS}`,
-      Processor: `${configuration.Processor}`,
-      Type: `${configuration.Type}`,
-      myUserName: `${myUserName}`,
-    }),
-  })
-    .then(handleErrors)
-    .then(response => response.json())
-    .then(body => body)
-    .catch(response => {
-      throw new Error(response.json());
-    });
-};
+    fetch(`${BASE_API}/createEnvironment`, {
+      headers: makeHeaders(),
+      body: JSON.stringify({
+        OS: `${configuration.OS}`,
+        Processor: `${configuration.Processor}`,
+        Type: `${configuration.Type}`,
+        myUserName: `${myUserName}`,
+      }),
+    })
+      .then(handleErrors)
+      .then(response => response.json())
+      .then(body => resolve(body))
+      .catch(response => {
+        reject(response.json());
+      });
+  });
