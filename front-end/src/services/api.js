6e07e6d20126f9fc0e36559ff32291ba972670e7
alex-fivetalent <alex.hanson@fivetalent.com>
2020-05-22 10:18:07 -0700

    58033958 Add Instance configuration lambda and data request

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -8,6 +8,7 @@ const makeHeaders = async () => {
 
   const sesh = await Auth.currentSession();
   const idToken = sesh.getIdToken().getJwtToken();
+  console.log(sesh.getAccessToken().payload.username);
 
   return {
     Authorization: idToken,
@@ -33,6 +34,80 @@ export const GetFromDatabase = () => {
     .then(response => response.json())
     .then(body => body)
     .catch(response => {
-      throw new Error(JSON.stringify(response));
+      throw new Error(response.json());
+    });
+};
+
+export const createUser = async (email, role) => {
+  const sesh = await Auth.currentSession();
+  const myUserName = sesh.getAccessToken().payload.username;
+
+  fetch(`${BASE_API}/createUser`, {
+    headers: makeHeaders(),
+    body: JSON.stringify({ email: `${email}`, role: `${role}`, myUserName: `${myUserName}` }),
+  })
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(body => body)
+    .catch(response => {
+      throw new Error(response.json());
+    });
+};
+
+export const disableUser = email => {
+  fetch(`${BASE_API}/disableUser`, {
+    headers: makeHeaders(),
+    body: JSON.stringify({ email: `${email}` }),
+  })
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(body => body)
+    .catch(response => {
+      throw new Error(response.json());
+    });
+};
+
+export const getInstanceConfigurations = () => {
+  fetch(`${BASE_API}/getInstanceConfigurations`, {
+    headers: makeHeaders(),
+  })
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(body => body)
+    .catch(response => {
+      throw new Error(response.json());
+    });
+};
+
+export const getEnvironments = () => {
+  fetch(`${BASE_API}/getEnvironments`, {
+    headers: makeHeaders(),
+  })
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(body => body)
+    .catch(response => {
+      throw new Error(response.json());
+    });
+};
+
+export const createEnvironmentRecord = async configuration => {
+  const sesh = await Auth.currentSession();
+  const myUserName = sesh.getAccessToken().payload.username;
+
+  fetch(`${BASE_API}/createEnvironment`, {
+    headers: makeHeaders(),
+    body: JSON.stringify({
+      OS: `${configuration.OS}`,
+      Processor: `${configuration.Processor}`,
+      Type: `${configuration.Type}`,
+      myUserName: `${myUserName}`,
+    }),
+  })
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(body => body)
+    .catch(response => {
+      throw new Error(response.json());
     });
 };
