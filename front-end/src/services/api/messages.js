d0ed4f1884484dd930d5da57003318d992a8f7e7
Alex Hanson <alex.hanson@fivetalent.com>
2020-06-02 13:57:39 -0700

    58404877 Add messages api

---
+++ front-end/src/services/api/messages.js
@@ -0,0 +1,54 @@
+import { Auth } from 'aws-amplify';
+
+const BASE_API = process.env.REACT_APP_BASE_API_URI;
+
+const makeHeaders = async () => {
+  console.log('Stage: ', process.env.REACT_APP_STAGE);
+  const sesh = await Auth.currentSession();
+  const idToken = await sesh.getIdToken().getJwtToken();
+  return {
+    Authorization: idToken,
+    'Content-Type': 'application/json',
+  };
+};
+
+function handleErrors(response) {
+  if (!response.ok) {
+    throw response;
+  }
+  return response;
+}
+
+export const getMessages = () =>
+  new Promise(async (resolve, reject) => {
+    fetch(`${BASE_API}/getMessages`, {
+      headers: await makeHeaders(),
+    })
+      .then(handleErrors)
+      .then(response => response.json())
+      .then(body => resolve(body.items))
+      .catch(response => {
+        reject(response);
+      });
+  });
+
+export const createMessage = async message =>
+  new Promise(async (resolve, reject) => {
+    const sesh = await Auth.currentSession();
+    const myUserName = sesh.getAccessToken().payload.username;
+
+    fetch(`${BASE_API}/createMesssage`, {
+      headers: await makeHeaders(),
+      body: JSON.stringify({
+        myUserName: `${myUserName}`,
+        ...message,
+      }),
+      method: 'POST',
+    })
+      .then(handleErrors)
+      .then(response => response.json())
+      .then(body => resolve(body))
+      .catch(response => {
+        reject(response);
+      });
+  });
