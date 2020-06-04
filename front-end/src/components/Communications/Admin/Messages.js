65c5ce25ea6d26f193f8cdb0766605259d0ab37c
alex-fivetalent <alex.hanson@fivetalent.com>
2020-06-04 15:02:24 -0700

    58366544 WIP for progress on chat

--- front-end/src/components/Communications/Admin/Messages.js
+++ front-end/src/components/Communications/Admin/Messages.js
@@ -3,17 +3,17 @@
 import React, { useState, useEffect } from 'react';
 import { getConversations } from '../../../services/api/messages';
 import arrowRight from '../../../assets/arrowRight.svg'
+import search from '../../../assets/search.svg';
 import moment from 'moment';
 import MessagesChat from './MessagesChat';
 
 export default function Messages() {
-	
+  const [ searchTerm, setSearchTerm ] = useState('');
 	const [ conversations, setConversations ] = useState([])
 	const [ selectedResearcher, setSelectedResearcher ] = useState(null)
 	const fetchConversation = async () => {
     try {
 			const response = await getConversations();
-      console.log(response);
       setConversations(response);
     } catch (error) {
       console.log(`Error fetching configurations${error}`);
@@ -24,9 +24,22 @@ export default function Messages() {
     fetchConversation();
     // eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
+
+	useEffect(() => {
+    // const filteredData = users.filter(
+    //   env => env.UserName.toLowerCase().includes(searchTerm.toLowerCase()) || env.Role.toLowerCase().includes(searchTerm.toLowerCase())
+    // );
+    // setFilteredUsers(filteredData);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchTerm]);
+
+  const handleSearch = async event => {
+    event.preventDefault();
+    const { value } = event.target;
+    setSearchTerm(value);
+  };
 	
 	const conversationsDisplay = conversations.map((mId, index) => {
-		console.log(mId)
 		return (
 		<div className="h-20 p-4 pr-6 text-gray-200" key={index} style={{backgroundColor: index % 2 ? '#1E2B34' : '#26343E'}}>
 			<div className="flex flex-row justify-between">
@@ -36,7 +49,7 @@ export default function Messages() {
 					<p>{moment(mId.Created).format('hh:mm A')}</p>
 				</div>
 			</div>
-				<p className="pt-2 text-sm leading-tight">
+				<p className="pt-2 leading-tight">
 					{mId.Payload.substr(0, 100).trim()}
 					<span>
 						<button className="pl-4 focus:outline-none" type="button" onClick={() => setSelectedResearcher(mId.ResearcherId_FK)}>
@@ -49,8 +62,18 @@ export default function Messages() {
 	})
 	
   return (
-	  <div>
-	  	{selectedResearcher ? <MessagesChat researcherId={selectedResearcher}/> : conversationsDisplay}
-	  </div>
+	  <>
+		<div className="relative h-20 bg-blue-800">
+			<input
+				className="p-1 pl-4 mt-6 ml-8 text-gray-200 bg-blue-800 border border-gray-200 border-solid rounded focus:outline-none"
+				type="text"
+				value={searchTerm}
+				name="name"
+				onChange={event => handleSearch(event)}
+			/>
+			<img className="absolute top-0 left-0 mt-8 ml-48" src={search} alt="" />
+		</div>
+	  {selectedResearcher ? <MessagesChat researcherId={selectedResearcher}/> : conversationsDisplay}
+	  </>
   );
 }
--- front-end/src/components/Communications/Admin/MessagesChat.js
+++ front-end/src/components/Communications/Admin/MessagesChat.js
@@ -1,10 +1,14 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/label-has-for */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import { getConversationById } from '../../../services/api/messages';
+import send from '../../../assets/send.svg';
+import { getConversationById, createMessage } from '../../../services/api/messages';
 
 const MessagesChat = ({ researcherId }) => {
   const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
   const fetchConversation = async () => {
     try {
       const response = await getConversationById(researcherId);
@@ -19,6 +23,26 @@ const MessagesChat = ({ researcherId }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      const researcher = messages[0].ResearcherId_FK;
+      const response = await createMessage(newMessage, researcher);
+      if (response) {
+        fetchConversation();
+      }
+      setNewMessage('');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      handleSubmit(event);
+    }
+  };
+
   const messagesDisplay = messages.map((mId, index) =>
     mId.ResearcherName === mId.SpeakerName ? (
       <div className="p-4 pr-20 text-gray-200 " key={index} style={{ backgroundColor: index % 2 ? '#1E2B34' : '#26343E' }}>
@@ -29,7 +53,7 @@ const MessagesChat = ({ researcherId }) => {
             <p>{moment(mId.Created).format('hh:mm A')}</p>
           </div>
         </div>
-        <p className="pt-2 text-sm leading-tight">{mId.Payload}</p>
+        <p className="pt-2 leading-tight">{mId.Payload}</p>
       </div>
     ) : (
       <div className="flex flex-col p-4 pl-20 pr-6 text-gray-200 " key={index} style={{ backgroundColor: index % 2 ? '#1E2B34' : '#26343E' }}>
@@ -40,12 +64,33 @@ const MessagesChat = ({ researcherId }) => {
           </div>
           <p className="uppercase">{mId.SpeakerName}</p>
         </div>
-        <p className="pt-2 text-sm leading-tight">{mId.Payload}</p>
+        <p className="pt-2 leading-tight">{mId.Payload}</p>
       </div>
     )
   );
 
-  return <div>{messagesDisplay}</div>;
+  return (
+    <div className="">
+      <div className="overflow-y-scroll" style={{ height: '27em' }}>
+        {messagesDisplay}
+        <div className="sticky" />
+      </div>
+      <div className="relative p-4" style={{ width: '26em' }}>
+        <input
+          placeholder="type to chat"
+          id="newMessage"
+          value={newMessage}
+          onChange={event => setNewMessage(event.target.value)}
+          className="p-2 pl-4 text-xs text-gray-200 bg-blue-600 border border-gray-200 border-solid rounded"
+          style={{ width: '33.5em' }}
+          onKeyPress={handleKeyPress}
+        />
+        <button type="submit" className="absolute top-0 right-0 pl-2 mt-6 mr-1 border-l" onClick={handleSubmit}>
+          <img src={send} alt="" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MessagesChat;
