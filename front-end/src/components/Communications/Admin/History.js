c041b92c5ecbeb1e37324ff9687c6d3ab52d6c82
Collin Robinson <collin.robinson@fivetalent.com>
2020-06-07 10:35:40 -0700

    Disabling announcments - admin

--- front-end/src/components/Communications/Admin/History.js
+++ front-end/src/components/Communications/Admin/History.js
@@ -2,32 +2,56 @@
 
 import React, { useState, useEffect } from 'react';
 import Spinner from  '../../Spinner.js'
-import { getAnnouncements } from '../../../services/api/announcements';
+import deleteIcon from '../../../assets/delete.svg';
+import editIcon from '../../../assets/edit.svg';
+import { getAnnouncements, disableAnnouncement } from '../../../services/api/announcements';
 
 export default function History() {
 	const [ announcements, setAnnouncements ] = useState([])
 	const [ isLoading, setIsLoading ] = useState(true);
+	
 	const fetchAnnouncements = async () => {
-    try {
-			const response = await getAnnouncements();
-      setAnnouncements(response);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(`Error fetching announcement: ${error}`);
-    }
-  };
+	    try {
+		  const response = await getAnnouncements();
+	      setAnnouncements(response);
+	      setIsLoading(false);
+	    } catch (error) {
+	      console.log(`Error fetching announcement: ${error}`);
+	    }
+  	};
+  	
+  	const handleDisable = async (id) => {
+	  	console.log(`disabling ${id}`)
+	  	setIsLoading(true);
+	  	try {
+		  const response = await disableAnnouncement(id);
+	      fetchAnnouncements();
+	      setIsLoading(false);
+	      
+	    } catch (error) {
+	      console.log(`Error disabling announcement: ${error}`);
+	    }
+  	};
+  	
+  	const handleUpdate = async (id) => {
+	  	
+  	};
 
   useEffect(() => {
     fetchAnnouncements();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps 
   }, []);
 	
 	const announcementDisplay = announcements.map((aId, index) => {
+		console.log(aId);
 		return(
 			<div className="p-4 pr-6 text-teal-500" key={index} style={{backgroundColor: index % 2 ? '#1E2B34' : '#26343E'}}>
 				<div className="flex justify-between flex-column">
-				<h6 className="uppercase"><span className="font-bold">Team Name:</span> {aId.Team}</h6>
 				<h6 className="uppercase"><span className="font-bold">Type:</span> {aId.Type}</h6>
+					<ul className="flex justify-between flex-column">
+						<li className="mr-2"><img onClick={() => handleUpdate(aId.Id)} src={editIcon} /></li>
+						<li className="mr-2"><img onClick={() => handleDisable(aId.Id)} src={deleteIcon} /></li>
+					</ul>
 				</div>
 				<p className="pt-2 text-sm leading-tight text-gray-200">{aId.Payload}</p>
 			</div>
@@ -36,7 +60,7 @@ export default function History() {
 
 	
   return (
-	  <div>
+	  <div className="relative">
 	  	{isLoading ? <Spinner /> : announcementDisplay};
 	  </div>
   );
