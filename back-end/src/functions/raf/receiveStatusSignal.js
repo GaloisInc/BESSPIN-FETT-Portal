b23f068939d5ef319007c377432faa477e601874
Brian McCall <brian.mccall@fivetalent.com>
2020-06-25 13:23:18 -0700

    instantiate ec2 in proper region; handle FETT startup failure

--- back-end/src/functions/raf/receiveStatusSignal.js
+++ back-end/src/functions/raf/receiveStatusSignal.js
@@ -1,10 +1,10 @@
 const aws = require('aws-sdk');
-const { CloudWatch, Database } = require('../../helpers');
+const { CloudWatch, Database, SsmHelper } = require('../../helpers');
 
-const ec2 = new aws.EC2();
 const db = new Database();
-
-const stopInstance = async instanceId => {
+const sqs = new aws.SQS();
+const stopInstance = async (instanceId, region) => {
+  const ec2 = new aws.EC2({ region });
   const params = {
     InstanceIds: [instanceId],
     DryRun: false,
@@ -19,26 +19,39 @@ const updateDBForTermination = async instanceId =>
   );
 const updateDBForStarted = async (instanceId, instanceIp, fpgaIp) => {
   console.log('updating for started');
-  await db.query(
+  return db.query(
     `UPDATE Environment set IpAddress = :instanceIp, FPGAIp = :fpgaIp, Status = "running" WHERE F1EnvironmentId = :instanceId`,
     { instanceId, instanceIp, fpgaIp }
   );
 };
-const getInstanceConfig = async (instanceId, instanceIp, fpgaIp) => {
-  await db.query(
-    `UPDATE Environment set IpAddress = :instanceIp, FPGAIp = :fpgaIp Status = "Started" WHERE Id = :instanceId`,
-    { instanceId, instanceIp, fpgaIp }
+const getEnvironmentConfig = async instanceId =>
+  db.query(`SELECT * FROM Environment WHERE F1EnvironmentId = :instanceId`, {
+    instanceId,
+  });
+
+const getInstanceConfig = async instanceId =>
+  db.query(
+    `SELECT ev.*, ic.*, u.* FROM Environment ev LEFT JOIN InstanceConfiguration ic on ic.Id = ev.Configuration_FK LEFT JOIN User u on u.Id = ev.CreatedBy_FK WHERE ev.F1EnvironmentId = :instanceId`,
+    { instanceId }
   );
+const getPassword = async username => {
+  const password = await SsmHelper.getParameter(
+    `/fettportal/credentials/${username}`
+  );
+  return password;
 };
+
 exports.handler = async event => {
   await db.makeConnection();
   for (const msg of event.Records) {
     console.log(msg);
     const message = JSON.parse(msg.body);
     const signal = message.job.reason.split('-').pop();
+    const envConf = await getEnvironmentConfig(message.instance.id);
+    console.log('environment config', envConf);
     if (signal === 'termination') {
       try {
-        await stopInstance(message.instance.id);
+        await stopInstance(message.instance.id, envConf[0].Region);
       } catch (e) {
         if (e.code === 'InvalidInstanceID.NotFound') {
           console.log('msg', e.message);
@@ -64,12 +77,38 @@ exports.handler = async event => {
         if (e.code === 'InvalidInstanceID.NotFound') {
           console.log('msg', e.message);
           console.log('Instance has already gone away');
+        } else if (e.code === 'IncorrectInstanceState') {
+          console.log('msg', e.message);
+          console.log('Instance is likely already in a stopped state');
         } else {
           throw e;
         }
       }
       // TODO build this out
-      await getInstanceConfig(message.instance.id);
+      let instanceConfigData = await getInstanceConfig(message.instance.id);
+      console.log('top', instanceConfigData);
+      [instanceConfigData] = instanceConfigData;
+      console.log('bottom', instanceConfigData);
+      console.log('instance config', instanceConfigData);
+      const pass = await getPassword(instanceConfigData.UserName);
+      const messageBody = {
+        Id: instanceConfigData.Id,
+        Type: instanceConfigData.Type,
+        OS: instanceConfigData.OS,
+        Processor: instanceConfigData.Processor,
+        ConfigurationKey: instanceConfigData.Configuration_FK,
+        Region: instanceConfigData.Region,
+        username: instanceConfigData.UserName,
+        password: pass,
+        creatorId: instanceConfigData.CreatedBy_FK,
+      };
+      const msgParams = {
+        QueueUrl: process.env.RESEARCHER_INITIALIZATION_QUEUE_URL,
+        MessageBody: JSON.stringify(messageBody),
+        MessageDeduplicationId: String(messageBody.creatorId),
+        MessageGroupId: String(messageBody.Id),
+      };
+      await sqs.sendMessage(msgParams).promise();
       await CloudWatch.deleteDashboards(message.instance.id);
     }
   }
