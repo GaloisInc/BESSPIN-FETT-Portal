5c4f8feb230bbc1235e08d0d4d041dcffc498b2b
Tristan Peng <pengt@catlin.edu>
2020-07-15 18:53:03 -0700

    Make entire width of sidebar linked

--- front-end/src/components/NavButton.js
+++ front-end/src/components/NavButton.js
@@ -5,13 +5,19 @@ import PropTypes from 'prop-types';
 export default function NavButton({ icon, routeName, path, currentRoute }) {
   return (
     <li
-      className={`flex flex-row h-8 items-center hover:bg-blue-600 ${currentRoute.includes(routeName) &&
-        'bg-blue-600'}`}
+      className={`flex flex-row h-8 items-center hover:bg-blue-600 ${
+        currentRoute.includes(routeName) ? 'bg-blue-600' : ''
+      }`}
     >
-      <div className={`w-1 h-8 ${currentRoute.includes(routeName) && 'bg-blue-300'}`} />
-      <img className="pr-3 ml-3 w-6" src={icon} alt="" />
       <Link className="uppercase" to={path}>
-        <h6 style={{ fontSize: 'calc(10px + (20 - 10) * ((100vw - 600px) / (2560 - 600)))' }}>{routeName}</h6>
+        <div className="grid grid-cols-12 items-center">
+          <div className="col-span-3">
+            <img className="pr-3 ml-3 w-6" src={icon} alt="" />
+          </div>
+          <div className="col-span-9">
+            <h6 style={{ fontSize: 'calc(10px + (20 - 10) * ((100vw - 600px) / (2560 - 600)))' }}>{routeName}</h6>
+          </div>
+        </div>
       </Link>
     </li>
   );
