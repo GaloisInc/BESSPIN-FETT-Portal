3f31088aba1aa36168dc293de8ed3172b28301b3
Tristan Peng <pengt@catlin.edu>
2020-07-16 15:46:52 -0700

    Add clock icon for provisioning status and add icon for error status

--- front-end/src/components/Instance/Alert.js
+++ front-end/src/components/Instance/Alert.js
@@ -3,16 +3,20 @@ import PropTypes from 'prop-types';
 import alert from '../../assets/alert.svg';
 import greenAlert from '../../assets/greenAlert.png';
 import amberAlert from '../../assets/amberAlert.png';
+import clock from '../../assets/clock.svg';
 
 const Alert = ({ status }) => {
   const statusColorSelect = () => {
     if (status === 'running') {
       return <img src={greenAlert} alt="green exclamation" />;
     }
-    if (status === 'provisioning' || status === 'terminating' || status === 'queueing') {
+    if (status === 'provisioning') {
+      return <img src={clock} alt="clock" />;
+    }
+    if (status === 'terminating' || status === 'queueing') {
       return <img src={amberAlert} alt="amber exclamation" />;
     }
-    if (status === 'terminated' || status === 'failed') {
+    if (status === 'terminated' || status === 'failed' || status === 'error') {
       return <img src={alert} alt="red exclamation" />;
     }
   };
