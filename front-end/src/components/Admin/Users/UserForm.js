e269823c98c80b7e090c68bc1d9514f3bf573831
Tristan Peng <pengt@catlin.edu>
2020-07-16 17:52:25 -0700

    Make user page content consistent height with flexbox

--- front-end/src/components/Admin/Users/UserForm.js
+++ front-end/src/components/Admin/Users/UserForm.js
@@ -65,8 +65,8 @@ const UserForm = ({ fetchUsers }) => {
   };
 
   return (
-    <div className="mr-6 bg-blue-600" style={{ width: '400px', minHeight: '630px' }}>
-      <h5 className="pt-16 pl-12 text-gray-200 uppercase">New Teams</h5>
+    <div className="bg-blue-600 h-full">
+      <h5 className="pt-8 pl-12 text-gray-200 uppercase">New Teams</h5>
       <form className="flex flex-col px-12 ml-4">
         <label htmlFor="teamNumber" className="mt-4 mb-1 text-gray-200 font-body">
           Number of Teams
