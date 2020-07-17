f2d3f4e6ea27f4768788dd79f414a5f74a04cf98
Alex Hanson <alex.hanson@fivetalent.com>
2020-07-17 08:21:35 -0700

    57202550 Add admin metrics table pulling from db

---
+++ front-end/src/components/Admin/AdminMetrics.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import InstanceManagement from '../Instance/InstanceManagement';
+import { getMetrics } from '../../services/api/metrics';
+import Spinner from '../Spinner';
+
+export default function AdminDash() {
+  const [metrics, setMetrics] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMetrics = async () => {
+    const results = await getMetrics();
+    setMetrics(results);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchMetrics();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const spinupsByType =
+    metrics.spinupsTotalByType &&
+    metrics.spinupsTotalByType.map((type, index) => (
+      <div key={index} className={`flex flex-row py-2 pl-4 ${index % 2 === 0 ? 'bg-blue-700' : 'bg-blue-600'}`}>
+        <div className="w-full ml-8 mr-8 ">
+          <p className="text-base text-teal-500 uppercase">{`${type.Type} | ${type.Processor} | ${type.OS}`}</p>
+        </div>
+        <p className="text-base text-gray-200 pr-16">{type.Count}</p>
+      </div>
+    ));
+
+  return (
+    <div className="h-full pt-6 pl-12">
+      <h3 className="text-gray-200 uppercase">dashboard</h3>
+      <p className="pt-4 text-gray-200">
+        This interface may be used to view all instances provisioned by any research team as well as to connect to AWS
+        logs/metrics for specific instances.
+      </p>
+      <div className="w-full mt-4">
+        <div className="relative mr-6 bg-blue-600 table-card w-full" style={{ maxWidth: '600px', minHeight: '630px' }}>
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <>
+              <div className="flex flex-row items-center justify-between pl-4 mt-4 mb-2">
+                <h5 className="font-medium text-gray-200 uppercase">Reporting Metrics</h5>
+                <div className="flex flex-row items-center mr-4" />
+              </div>
+              <div className="flex flex-row py-2 bg-blue-700">
+                <div className="w-full ml-8 mr-8 ">
+                  <p className="text-base text-teal-500 uppercase">Launches</p>
+                </div>
+                <p className="text-base pr-16 text-gray-200">{metrics && metrics.spinups}</p>
+              </div>
+              <div className="flex flex-row py-2 bg-blue-600">
+                <div className="w-full ml-8 mr-8 ">
+                  <p className="text-base text-teal-500 uppercase">Terminations</p>
+                </div>
+                <p className="text-base pr-16 text-gray-200">{metrics && metrics.terminationsTotal}</p>
+              </div>
+              <div className="flex flex-row py-2 bg-blue-700">
+                <div className="w-full ml-8 mr-8 ">
+                  <p className="text-base text-teal-500 uppercase">Errors Launching</p>
+                </div>
+                <p className="text-base pr-16 text-gray-200">{metrics && metrics.spinupErrorsTotal}</p>
+              </div>
+              <div className="flex flex-row py-2 bg-blue-600">
+                <div className="w-full ml-8 mr-8 ">
+                  <p className="text-base text-teal-500 uppercase">Launches By Type</p>
+                </div>
+              </div>
+              <div>{spinupsByType}</div>
+              <p className="text-xs pr-16 pl-4 text-gray-200">{`* Data include activity since ${moment(
+                '07-15-20 10:00:00 PDT'
+              ).format('l, LT')} and do not include test profile activity`}</p>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
--- front-end/src/components/NavButton.js
+++ front-end/src/components/NavButton.js
@@ -2,21 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import AssessmentOutlinedIcon from '@material-ui/icons/AssessmentOutlined';
 
 export default function NavButton({ icon, routeName, path, currentRoute }) {
+  const Icon = () => {
+    if (routeName === 'broadcast') {
+      return (
+        <div className="pr-3 ml-3">
+          <ChatBubbleOutlineIcon style={{ fontSize: 15 }} />
+        </div>
+      );
+    }
+    if (routeName === 'metrics') {
+      return (
+        <div className="pr-3 ml-3">
+          <AssessmentOutlinedIcon style={{ fontSize: 15 }} />
+        </div>
+      );
+    }
+    return <img className="pr-3 ml-3 w-6" src={icon} alt="" />;
+  };
+
   return (
     <li
       className={`flex flex-row h-8 pr-2 items-center hover:bg-blue-600 ${currentRoute.includes(routeName) &&
         'bg-blue-600'}`}
     >
       <div className={`w-1 h-8 ${currentRoute.includes(routeName) && 'bg-blue-300'}`} />
-      {routeName === 'broadcast' ? (
-        <div className="pr-3 ml-3">
-          <ChatBubbleOutlineIcon style={{ fontSize: 15 }} />
-        </div>
-      ) : (
-        <img className="pr-3 ml-3 w-6" src={icon} alt="" />
-      )}
+      <Icon />
       <Link className="uppercase" to={path}>
         <h6>{routeName}</h6>
       </Link>
