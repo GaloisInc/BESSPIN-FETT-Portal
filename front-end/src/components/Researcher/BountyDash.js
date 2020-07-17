3d99cb74605de0e9b59240c40f2a90930b3d5643
Tristan Peng <pengt@catlin.edu>
2020-07-17 15:58:30 -0700

    Give dashboard same left padding as learn and launch tabs

--- front-end/src/components/Researcher/BountyDash.js
+++ front-end/src/components/Researcher/BountyDash.js
@@ -4,7 +4,7 @@ import Comms from '../Communications/Card';
 
 export default function BountyDash() {
   return (
-    <div className="pt-6 px-8">
+    <div className="pt-6 px-12">
       <h3 className="text-gray-200 uppercase">dashboard</h3>
       <p className="pt-4 text-gray-200">
         Use the interface below to understand the status of any instances you have launched, and to manipulate them.
