b7dc50c8dc1e324eba3bdb612aea0bc3962dc4bd
Tristan Peng <pengt@catlin.edu>
2020-07-20 16:14:44 -0700

    Add max width on dashboards

--- front-end/src/components/Researcher/BountyDash.js
+++ front-end/src/components/Researcher/BountyDash.js
@@ -4,12 +4,12 @@ import Comms from '../Communications/Card';
 
 export default function BountyDash() {
   return (
-    <div className="pt-6 px-12 overflow-y-scroll" style={{ height: '85vh' }}>
+    <div className="pt-6 px-12 overflow-y-scroll" style={{ height: '85vh', maxWidth: 1440 }}>
       <h3 className="text-gray-200 uppercase">dashboard</h3>
       <p className="pt-4 text-gray-200">
         Use the interface below to understand the status of any instances you have launched, and to manipulate them.
       </p>
-      <div className="w-full mt-4 grid grid-cols-12 gap-4">
+      <div className="mt-4 grid grid-cols-12 gap-4">
         <InstanceHistory />
         <Comms userType="researcher" />
       </div>
