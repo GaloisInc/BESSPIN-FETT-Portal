7292200a78f06af255d040267a8019205b493228
Tristan Peng <pengt@catlin.edu>
2020-07-20 11:09:16 -0700

    Resolve #401

--- front-end/src/components/Instance/Alert.js
+++ front-end/src/components/Instance/Alert.js
@@ -1,32 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Icon from '@material-ui/core/Icon';
 import alert from '../../assets/alert.svg';
 import greenAlert from '../../assets/greenAlert.png';
 import amberAlert from '../../assets/amberAlert.png';
 import clock from '../../assets/clock.svg';
+import stop from '../../assets/stop.svg';
 
 const Alert = ({ status }) => {
   const statusColorSelect = () => {
     switch (status) {
       case 'running':
         return <img src={greenAlert} alt="green exclamation" />;
+      case 'queueing':
       case 'provisioning':
         return <img src={clock} alt="clock" />;
       case 'terminating':
         return <img src={amberAlert} alt="amber exclamation" />;
-      case 'queueing':
-        return (
-          <Icon className="align-middle" style={{ fontSize: 15, color: '#deb200' }}>
-            hourglass_full
-          </Icon>
-        );
       case 'terminated':
-        return (
-          <Icon className="align-middle" style={{ fontSize: 20, color: '#e10000' }}>
-            stop
-          </Icon>
-        );
+        return <img src={stop} alt="stop" />;
       case 'failed':
       case 'error':
         return <img src={alert} alt="red exclamation" />;
