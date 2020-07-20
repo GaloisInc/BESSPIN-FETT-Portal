e4ebf78259a0096d3baa90532db456e14b498ce3
Tristan Peng <pengt@catlin.edu>
2020-07-20 11:33:02 -0700

    Add flex wrapping, make sidebar icons responsive, and add scrolling

--- front-end/src/components/Communications/Researcher/Navigation.js
+++ front-end/src/components/Communications/Researcher/Navigation.js
@@ -8,7 +8,7 @@ const Navigation = ({update, setNav}) => {
 	const NavStyle = 'border-solid border-b-4 border-teal-600'
 
 return(
-  <ul className="flex flex-column justify-start">
+  <ul className="flex flex-column flex-wrap justify-start">
     <li className={`mr-4 cursor-pointer ${setNav==="Announcements"? NavStyle : ''}`}  onClick={() => update('Announcements')}>Announcements</li>
     <li className={`mr-4 cursor-pointer ${setNav==="ResMessages"? NavStyle : ''}`}  onClick={() => update('ResMessages')}>Messages</li>
   </ul>
