58c666cb8c7bf02365af9f407e65afd5d395edad
Alex Hanson <alex.hanson@fivetalent.com>
2020-08-26 12:10:09 -0700

    Add back reset for FETT Target Debug

--- back-end/src/functions/raf/createResearcherInstance.js
+++ back-end/src/functions/raf/createResearcherInstance.js
@@ -78,7 +78,7 @@ const getUserData = (f1Config, iName) => {
   git stash
   popd
   git pull    
-  git checkout develop
+  git checkout dev-portal
   git pull        
   git submodule update --init --recursive    
   pushd SSITH-FETT-Binaries    
