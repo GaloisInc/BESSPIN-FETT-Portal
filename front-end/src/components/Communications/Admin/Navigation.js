3b24f37b2ad9b2adf7629dd199433f2c30cf641d
Tristan Peng <pengt@catlin.edu>
2020-08-28 13:00:55 -0700

    Fix errors on /adminportal/broadcast

--- front-end/src/components/Communications/Admin/Navigation.js
+++ front-end/src/components/Communications/Admin/Navigation.js
@@ -5,8 +5,7 @@ import PropTypes from 'prop-types';
 
 const Navigation = ({update, setNav}) => {
 	
-	const NavStyle = 'border-solid border-b-4 border-teal-600'
-	
+	const NavStyle = 'border-solid border-b-4 border-teal-600';
 	
 	return (
 	  <ul className="flex flex-column flex-wrap justify-start">
@@ -19,6 +18,6 @@ const Navigation = ({update, setNav}) => {
 export default Navigation;
 
 Navigation.propTypes = {
-  update: PropTypes.function,
+  update: PropTypes.func,
   setNav: PropTypes.string,
 };
